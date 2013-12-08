6dcac1a342b4ed46280e8f08c5fb257bf26eef25
David Brady <dbrady@shinybit.com>
2013-12-08 11:37:03 -0700

    Uncomment at least ONE of the text-mandelbrots :-)

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -1,6 +1,6 @@
 # This is the highest-contrast, and I think, prettiest, version. It
 # uses .'s and #'s. 136 chars.
-# 60.times{|a|puts((0..240).map{|b|x=y=i=0;until(x*x+y*y>4||i==99);x,y,i=x*x-y*y+b/120.0-1.5,2*x*y+a/30.0-1,i+1;end;i==99?'#':'.'}*'');}
+60.times{|a|puts((0..240).map{|b|x=y=i=0;until(x*x+y*y>4||i==99);x,y,i=x*x-y*y+b/120.0-1.5,2*x*y+a/30.0-1,i+1;end;i==99?'#':'.'}*'');}
 
 
 # Special thanks to @brahbur http://twitter.com/brahbur for this
